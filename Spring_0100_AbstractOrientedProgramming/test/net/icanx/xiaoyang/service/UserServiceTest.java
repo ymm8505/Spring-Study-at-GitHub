30c08eef07c566bf56dea82a92b02395e318cbdd
ymm8505 <snjeje@163.com>
2015-06-16 22:35:15 +0800

    完成jdom 解析Xml 1、jdom.jar 和 jdom.source 关联 2、SAXBuilder 解析找classpath路径下的xml文件 加上扩展名

--- Spring_0100_AbstractOrientedProgramming/test/net/icanx/xiaoyang/service/UserServiceTest.java
+++ Spring_0100_AbstractOrientedProgramming/test/net/icanx/xiaoyang/service/UserServiceTest.java
@@ -1,14 +1,26 @@
 package net.icanx.xiaoyang.service;
 
-import net.icanx.xiaoyang.model.User;
+import java.io.IOException;
 
+import net.icanx.xiaoyang.dao.UserDAO;
+import net.icanx.xiaoyang.model.User;
+import net.icanx.xiaoyang.spring.BeanFactory;
+import net.icanx.xiaoyang.spring.ClassPathXmlApplicationContext;
+
+import org.jdom.JDOMException;
 import org.junit.Test;
 
 public class UserServiceTest {
 
 	@Test
-	public void test() {
-		UserService service = new UserService();
+	public void test() throws JDOMException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		
+		BeanFactory factory = new ClassPathXmlApplicationContext();
+		UserDAO userDao = (UserDAO)factory.getBean("u");
+		
+		UserService service = new UserService(); 	
+		service.setUserDAO(userDao);
+		
 		User u = new User();
 		service.save(u);
 	}
